aa51a827cf04ced27dbfdc69d68842f49fcf1b9a
Diana Mocanu <58723126+dianamocanu05@users.noreply.github.com>
2021-05-20 22:14:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.use(express.json());
 app.use("/api/users", userRouter);
 
 
-app.listen(process.env.APP_PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port " + process.env.APP_PORT + "...");
 });
